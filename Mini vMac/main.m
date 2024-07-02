ff47d4e187b0e9634bc053c4385430edbf471377
Jason Sewell <124761103+JasonInRVA@users.noreply.github.com>
2024-07-02 18:55:58 -0400

    Update main.m

--- Mini vMac/main.m
+++ Mini vMac/main.m
@@ -13,4 +13,5 @@ int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, @"MNVMApplication", NSStringFromClass([AppDelegate class]));
     }
-}
+} 
+
